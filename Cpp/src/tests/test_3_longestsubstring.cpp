568d50a0d7390d7ab5ce93f335be22193ba67a90
inque <romanvgrigorii@gmail.com>
2025-03-25 08:03:40 -0700

    Solving problem 4 in Cpp

--- Cpp/src/tests/test_3_longestsubstring.cpp
+++ Cpp/src/tests/test_3_longestsubstring.cpp
@@ -3,12 +3,21 @@
 #include "helpers.hpp"
 #include <vector>
 #include <map>
+#include <string>
 
 using namespace::std;
 
 
 TEST(TestSuite, LengthOfLongestSubstring) { 
-    auto res = lengthOfLongestSubstring("abcabcbb");
-    auto exp = 4;
-    COMPARE_EQ(res, exp);
+    int res;
+    int exp;
+    res = lengthOfLongestSubstring("abcabcbb");
+    exp = 3;
+    EXPECT_EQ(res, exp);
+    res = lengthOfLongestSubstring("bbbbb");
+    exp = 1;
+    EXPECT_EQ(res, exp);
+    res = lengthOfLongestSubstring("pwwkew");
+    exp = 3;
+    EXPECT_EQ(res, exp);
 }
